f0b79697fc87aeb159fa4c46e511774b0feae970
Igor <igor@Igors-iMac.local>
2018-04-09 23:32:56 +0300

    Initial commit

--- app/src/main/java/com/nonexistentware/igorsinchuk/samplevideoplayer/Main.java
+++ app/src/main/java/com/nonexistentware/igorsinchuk/samplevideoplayer/Main.java
@@ -1,14 +1,19 @@
 package com.nonexistentware.igorsinchuk.samplevideoplayer;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,29 +23,30 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class VideoStoredInSDCard extends Activity {
+public class Main extends Activity {
 	private Cursor videoCursor;
 	private int videoColumnIndex;
 	ListView videoList;
 	int count;
-	String[] thumbColumns = { MediaStore.Video.Thumbnails.DATA,
-			MediaStore.Video.Thumbnails.VIDEO_ID };
+	String[] thumbColumns = {MediaStore.Video.Thumbnails.DATA,
+			MediaStore.Video.Thumbnails.VIDEO_ID};
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		videoGrid();
+		checkUserPermission();
 	}
 
-	@SuppressWarnings("deprecation")
 	private void videoGrid() {
-		String[] proj = { MediaStore.Video.Media._ID,
+		String[] proj = {MediaStore.Video.Media._ID,
 				MediaStore.Video.Media.DATA,
 				MediaStore.Video.Media.DISPLAY_NAME,
-				MediaStore.Video.Media.SIZE };
-		videoCursor = managedQuery(MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+				MediaStore.Video.Media.SIZE};
+		videoCursor = getContentResolver().query(MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
 				proj, null, null, null);
 		count = videoCursor.getCount();
 		videoList = (ListView) findViewById(R.id.PhoneVideoList);
@@ -50,14 +56,14 @@ public class VideoStoredInSDCard extends Activity {
 
 	private OnItemClickListener videogridlistener = new OnItemClickListener() {
 		public void onItemClick(AdapterView parent, View v, int position,
-				long id) {
+								long id) {
 			videoColumnIndex = videoCursor
 					.getColumnIndexOrThrow(MediaStore.Video.Media.DATA);
 			videoCursor.moveToPosition(position);
 			String filename = videoCursor.getString(videoColumnIndex);
-			Intent intent = new Intent(VideoStoredInSDCard.this,
+			Intent intent = new Intent(Main.this,
 					ViewVideo.class);
-			intent.putExtra("videofilename", filename);
+			intent.putExtra("VideoFile", filename);
 			startActivity(intent);
 		}
 	};
@@ -91,8 +97,6 @@ public class VideoStoredInSDCard extends Activity {
 				holder = new ViewHolder();
 				holder.txtTitle = (TextView) convertView
 						.findViewById(R.id.txtTitle);
-				holder.txtSize = (TextView) convertView
-						.findViewById(R.id.txtSize);
 				holder.thumbImage = (ImageView) convertView
 						.findViewById(R.id.imgIcon);
 
@@ -106,17 +110,16 @@ public class VideoStoredInSDCard extends Activity {
 				// id += " Size(KB):" +
 				// videoCursor.getString(videoColumnIndex);
 				holder.txtTitle.setText(id);
-				holder.txtSize.setText(" Size(KB):"
-						+ videoCursor.getString(videoColumnIndex));
 
-				String[] proj = { MediaStore.Video.Media._ID,
+				String[] proj = {MediaStore.Video.Media._ID,
 						MediaStore.Video.Media.DISPLAY_NAME,
-						MediaStore.Video.Media.DATA };
-				@SuppressWarnings("deprecation")
-				Cursor cursor = managedQuery(
+						MediaStore.Video.Media.DATA};
+
+
+				Cursor cursor = getContentResolver().query(
 						MediaStore.Video.Media.EXTERNAL_CONTENT_URI, proj,
 						MediaStore.Video.Media.DISPLAY_NAME + "=?",
-						new String[] { id }, null);
+						new String[]{id}, null);
 				cursor.moveToFirst();
 				long ids = cursor.getLong(cursor
 						.getColumnIndex(MediaStore.Video.Media._ID));
@@ -134,11 +137,41 @@ public class VideoStoredInSDCard extends Activity {
 			return convertView;
 		}
 	}
+	// methods for check permission
+
+
+
+	private void checkUserPermission(){
+		if(Build.VERSION.SDK_INT>=23){
+			if(ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+					!= PackageManager.PERMISSION_GRANTED){
+				requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},123);
+				return;
+			}
+		}
+
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+		switch (requestCode) {
+			case 123:
+				if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+					videoGrid();//
+				} else {
+					Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+					checkUserPermission();
+				}
+				break;
+			default:
+				super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+		}
+	}
 
 	static class ViewHolder {
 
 		TextView txtTitle;
-		TextView txtSize;
 		ImageView thumbImage;
 	}
 }
--- app/src/main/java/com/nonexistentware/igorsinchuk/samplevideoplayer/ViewVideo.java
+++ app/src/main/java/com/nonexistentware/igorsinchuk/samplevideoplayer/ViewVideo.java
@@ -1,9 +1,15 @@
 package com.nonexistentware.igorsinchuk.samplevideoplayer;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.widget.MediaController;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 public class ViewVideo extends Activity {
@@ -14,7 +20,7 @@ public class ViewVideo extends Activity {
             super.onCreate(savedInstanceState);
             Intent play = getIntent();
             Bundle extras = play.getExtras();
-            filename = extras.getString("videofilename");
+            filename = extras.getString("VideoFile");
             setContentView(R.layout.activity_view);
             videoView = (VideoView) findViewById(R.id.videoView);
             videoView.setVideoPath(filename);
